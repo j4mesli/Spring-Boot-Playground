3ba37e646871a769e3ae316b3ecdc2cdb151f9da
James Li <34687158+j4mesli@users.noreply.github.com>
2023-02-11 14:59:59 -0500

    Delete demo directory

--- demo/src/main/java/Coach.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-public interface Coach {
-	
-	public String getDailyWorkout();
-	
-	public String getDailyFortune();
-	
-}
--- demo/src/main/java/FortuneService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-public interface FortuneService {
-	
-	public String getFortune();
-}
